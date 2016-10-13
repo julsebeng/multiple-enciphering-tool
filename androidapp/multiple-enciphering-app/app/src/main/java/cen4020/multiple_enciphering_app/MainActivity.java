b66049e2ad46fa3b865e28fcd160c2dc9413decc
Nicholas Purdy <nicholaswpurdy@gmail.com>
2016-10-13 18:08:30 -0400

    encrypt/decrypt activites started - nick/keaton
    waiting on cipher activity

--- androidapp/multiple-enciphering-app/app/src/main/java/cen4020/multiple_enciphering_app/MainActivity.java
+++ androidapp/multiple-enciphering-app/app/src/main/java/cen4020/multiple_enciphering_app/MainActivity.java
@@ -1,13 +1,43 @@
 package cen4020.multiple_enciphering_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button encryptBtn;
+    Button decryptBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encryptBtn = (Button) findViewById(R.id.encryptBtn);
+        decryptBtn = (Button) findViewById(R.id.decryptBtn);
+
+        encryptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), EncryptActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
+
+        decryptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent myIntent = new Intent(v.getContext(), DecryptActivity.class);
+                //startActivity(myIntent);
+                Toast.makeText(getApplicationContext(), "You clicked the decrypt button.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
