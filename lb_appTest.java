38b8631d86fd518b73c6caff4d51849f7ac3e495
Julian Engel <jsebastianengel@gmail.com>
2016-10-18 12:44:39 -0400

    Some additional unit tests

--- lb_appTest.java
+++ lb_appTest.java
@@ -7,8 +7,12 @@ import static org.junit.Assert.*;
  */
 public class lb_appTest {
 
+    /* ***********************************************************
+     * Single parameter tests
+    /*************************************************************/
+
     @Test
-    public void test() {
+    public void testEnc() {
         String[] testArgs = new String[] {"enc"};
 
         try {
@@ -19,4 +23,77 @@ public class lb_appTest {
             assertEquals("No cipher was given.", testErr);
         }
     }
+
+    @Test
+    public void testEncode() {
+        String[] testArgs = new String[] {"encode"};
+
+        try {
+            lb_app.main(testArgs);
+        }
+        catch(Exception e) {
+            String testErr = e.getMessage();
+            assertEquals("No cipher was given.", testErr);
+        }
+    }
+
+    @Test
+    public void testDec() {
+        String[] testArgs = new String[] {"dec"};
+
+        try {
+            lb_app.main(testArgs);
+        }
+        catch(Exception e) {
+            String testErr = e.getMessage();
+            assertEquals("No cipher was given.", testErr);
+        }
+    }
+
+    @Test
+    public void testDecode() {
+        String[] testArgs = new String[] {"decode"};
+
+        try {
+            lb_app.main(testArgs);
+        }
+        catch(Exception e) {
+            String testErr = e.getMessage();
+            assertEquals("No cipher was given.", testErr);
+        }
+
+    }
+
+    @Test
+    public void test_o() {
+        String[] testArgs = new String[] {"-o", "file"};
+
+        try {
+            lb_app.main(testArgs);
+        }
+        catch(Exception e) {
+            String testErr = e.getMessage();
+            assertEquals("Invalid arguments.", testErr);
+        }
+
+    }
+
+    /* ***********************************************************
+     * Actually useful tests
+    /*************************************************************/
+
+    public void test_j() {
+        String[] testArgs = new String[] {"-o", };
+
+        try {
+            lb_app.main(testArgs);
+        }
+        catch(Exception e) {
+            String testErr = e.getMessage();
+            assertEquals("Invalid arguments.", testErr);
+        }
+
+    }
+
+
 }
