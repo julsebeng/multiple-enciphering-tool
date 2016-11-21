552ce8cee58de3aa535d48fc96c2827047f41bcb
jse13 <jsebastianengel@gmail.com>
2016-11-21 09:03:13 -0500

    app now includes cipher library; use import cipher.* when needed

--- ChatPrototype/app/src/main/java/com/julianengel/smsexploratoryprototype/TestLibActivity.java
+++ ChatPrototype/app/src/main/java/com/julianengel/smsexploratoryprototype/TestLibActivity.java
@@ -2,6 +2,7 @@ package com.julianengel.smsexploratoryprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import cipher.*;
 
 public class TestLibActivity extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class TestLibActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_lib);
+
+        Cipher cipher = new Cipher() {
+            @Override
+            public Object clone() throws CloneNotSupportedException {
+                return null;
+            }
+
+            @Override
+            public boolean equals(Cipher cipher) {
+                return false;
+            }
+
+            @Override
+            public String encrypt(String s) throws Exception {
+                return null;
+            }
+
+            @Override
+            public String decrypt(String s) throws Exception {
+                return null;
+            }
+        };
     }
+
 }
