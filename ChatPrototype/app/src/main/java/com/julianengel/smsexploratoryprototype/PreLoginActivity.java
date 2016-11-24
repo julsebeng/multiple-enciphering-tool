542aa74e914ec053ff288dc98f234fa82322187e
Thai Flowers <THF15B@my.fsu.edu>
2016-11-24 00:02:31 -0500

    Holding changes temporarily for manual merge with Joseph's work

---
+++ ChatPrototype/app/src/main/java/com/julianengel/smsexploratoryprototype/PreLoginActivity.java
@@ -0,0 +1,14 @@
+package com.julianengel.smsexploratoryprototype;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Thai Flowers on 11/23/2016.
+ */
+
+public class PreLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+}
