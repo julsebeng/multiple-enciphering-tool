4523b2e22bfe7632f2f43c81749486e989a94846
Julian Engel <me@julianengel.com>
2016-11-30 22:52:23 -0500

    it's the final commit!

---
+++ Labyrinthine/LabyrinthineApp/app/src/main/java/com/fsu/cen4020/cipher/MessageListAdapter.java
@@ -0,0 +1,234 @@
+package com.fsu.cen4020.cipher;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.squareup.picasso.Picasso;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import com.parse.ParseUser;
+
+import cipher.CipherSequence;
+
+/**
+ * Created by Julian and Chris on 10/25/16.
+ * Major revisions by Thai on 11/13/16
+ */
+
+/* This class defines how our data will interact with the ListView; it
+ * describes what data is in each item and how it's structured.
+ * The adapter is responsible for creating a View for the ListView to use for
+ * each of the items contained within it.
+ */
+public class MessageListAdapter extends ParseQueryAdapter<ParseObject> {
+    static final String TAG = ChatActivity.class.getSimpleName();
+    private CipherSequence cipherSequence;
+    static final int MAX_MESSAGES_TO_SHOW = 50;
+    private String CHAT_ID;
+    private String userId;
+    private Boolean privy;
+
+    public MessageListAdapter(Context context, final String chatId, final Boolean privy, final CipherSequence cipherSequence) {
+        super(context, new ParseQueryAdapter.QueryFactory<ParseObject>() {
+            public ParseQuery create() {
+                ParseQuery query = new ParseQuery(Message.class);
+                query.whereEqualTo("chatId", chatId);
+                query.setLimit(MAX_MESSAGES_TO_SHOW);
+                query.orderByAscending("createdAt");
+                return query;
+            }
+        });
+
+        CHAT_ID = chatId;
+        userId = ParseUser.getCurrentUser().getObjectId();
+        this.cipherSequence = cipherSequence;
+        this.privy = privy;
+    }
+
+    public String getChatId() { return CHAT_ID ; }
+
+    @Override
+    public View getItemView(ParseObject object, View convertView, ViewGroup parent) {
+
+		/* If an item in the list has already been created (for example, if we
+		 * want to display a message that's already been rendered in the list)
+		 * then just recycle it instead of inflating a new one.
+		 */
+        if(convertView == null) {
+
+			/* If there is no older version of the data, inflate a new one.
+			 * So if we get a new message that hasn't been displayed before,
+			 * make a new entry in the ListView.
+			 */
+
+			/* Parameters for inflate():
+			 * 	R.layout.chat_item: int resource ID of the resource to load.
+			 * 	parent: ViewGroup passed in that is the parent of the newly
+			 * 		inflated View. In this case this should be the lvChat
+			 * 		ListView defined in activity_chat.xml.
+			 * 	false: determines if the new View should be attached and
+			 * 		displayed in the parent view. In this case we don't want
+			 * 		to do that quite yet so we set it to false.
+			 */
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.chat_item, parent, false);
+
+			/* This is a class that we define that will hold data for each
+			 * View in the View; basically it's a method of allowing a View to
+			 * have its own data associated with it, even if it's not currently
+			 * being displayed.
+			 */
+            final ViewHolder holder = new ViewHolder();
+            holder.imageOther = (ImageView)convertView.findViewById(R.id.ivProfileOther);
+            holder.imageMe = (ImageView)convertView.findViewById(R.id.ivProfileMe);
+            holder.body = (TextView)convertView.findViewById(R.id.tvBody);
+
+			/* Set our new tag object on the newly created convertView.
+			 */
+            convertView.setTag(holder);
+        }
+
+        super.getItemView(object, convertView, parent);
+
+        final Message message = (Message)object;
+
+		/* Get the data that's been associated with that particular message.
+		 * This contains the references for that message item's layout fields:
+		 * ivProfileOther, ivProfileMe, tvBody.
+		 */
+        final ViewHolder holder = (ViewHolder)convertView.getTag();
+
+		/* We test to see if the message was from our current user or some
+		 * other user. This affects the user image justification.
+		 */
+        final boolean isMe =
+                (message != null) &&
+                (message.getUserId() != null) &&
+                 message.getUserId().equals(userId);
+
+        /* Show/hide image based on logged in user.
+         * Justify local user's pic to the right, left to other users.
+		 */
+        if(isMe) {
+			/* Make current message's ivProfileMe ImageView visible.
+			 */
+            holder.imageMe.setVisibility(VISIBLE);
+
+			/* Make the current message's ivProfileOther ImageView invisible.
+			 */
+            holder.imageOther.setVisibility(GONE);
+
+			/* Justify the tvBody TextView to the right and center it.
+			 */
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+        }
+        else {
+			/* If the message isn't from the current user, make the current
+			 * user's picture invisible, the other user's picture visible,
+			 * and justify their text to the left.
+			 */
+            holder.imageOther.setVisibility(VISIBLE);
+            holder.imageMe.setVisibility(GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        }
+
+		/* Get the appropriate ImageView to draw a profile picture in.
+		 */
+        final ImageView profileView = isMe ? holder.imageMe : holder.imageOther;
+
+		/* Picasso is a 3rd party library for downloading, transforming, and
+		 * caching images. We use this because we're generating profile pictures
+		 * using the website gravatar.com based on a hash of the userId. This
+		 * generates a unique profile picture for each user that doesn't have
+		 * to be stored externally.
+		 */
+		/* load(): fetches an image from the specified URL. This is calculated
+		 * 	by the function below.
+		 * into(): loads the fetched image into the specified ImageView.
+		 */
+        Picasso.with(getContext()).load(getProfileUrl(message.getUserId())).into(profileView);
+
+        // Fetch and decrypt message text
+        String messBody = message.getBody();
+        if (privy) {
+            try {
+                messBody = cipherSequence.decrypt(messBody);
+            }
+            catch (Exception ex) {
+                Log.e(TAG, "Error in cipherSequence in MessageListAdapter");
+            }
+        }
+
+		/* Set the text of the message View's TextView to the body of the stored
+		 * Message object.
+		 */
+
+        holder.body.setText(messBody);
+        return convertView;
+
+    }
+
+    /* Creates image based on the hash from userId. This is useful because we
+	 * can programatically generate and fetch profile pictures unique to each
+	 * user.
+	 */
+    private static String getProfileUrl(final String userId) {
+        String hex = "";
+        try {
+
+			/* Returns a MessgageDigest object that implements MD5 algorithm.
+			 */
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+
+			/* digest() takes in a byte[] parameter, hashes it, and returns
+			 * a byte[] hash.
+			 * getBytes() takes a string and converts it to a byte[] stream.
+			 */
+            final byte[] hash = digest.digest(userId.getBytes());
+
+			/* Turns the byte[] hash into an integer so that we can absolute
+			 * value it and transform it into a 16-character string.
+			 */
+            final BigInteger bigInt = new BigInteger(hash);
+
+			/* For clarification:
+			 * An MD5 hash is always 128 bits long.
+			 * Characters in Java are 1 byte long, or 8 bits.
+			 * So, 128/8 = 16 characters needed to store the entire MD5 hash.
+			 *
+			 */
+            hex = bigInt.abs().toString(16);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+		/* Return a string pointing to the particular URL that Picasso can use
+		 * to download an image that's generated based on our hashed value.
+		 */
+        return "http://www.gravatar.com/avatar/" + hex + "?d=identicon";
+    }
+
+	/* Class to use as a tag for our message Views. This allows the information
+	 * about each message's unique layout elements (the ivProfileOther,
+	 * ivProfileMe, tvBody) to be saved along with the item.
+	 */
+    final class ViewHolder {
+        public ImageView imageOther;
+        public ImageView imageMe;
+        public TextView body;
+    }
+}
---
+++ Labyrinthine/LabyrinthineApp/app/src/main/java/com/fsu/cen4020/cipher/StartupActivity.java
@@ -0,0 +1,107 @@
+package com.fsu.cen4020.cipher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by crow(gridnaught) ((Joseph)) on 11/24/16.
+ */
+
+public class StartupActivity extends AppCompatActivity {
+    static final String TAG = StartupActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Context context = getApplicationContext();
+
+        //supposed to take stuff out of apk into phone if first time
+        //if (prefs.getBoolean("FIRST_RUN", true)) {
+
+            try {
+                AssetManager mngr = getAssets();
+                // get list of files in the cipher-library subdirectory of assets
+                String[] cyphFiles = mngr.list("cipher-library");
+                // destination path, something like /data/data/<package_name>/files/
+                String destPath =
+                        context.getFilesDir().getPath()
+                        + File.separator
+                        + "cipher-library";
+                // Make File out of string, and make sure the File represents a directory
+                File destDir = new File(destPath);
+                if (!destDir.exists())
+                    destDir.mkdirs();
+                for (int i = 0; i < cyphFiles.length; i++) {
+                    String destFile =
+                        destPath
+                        + File.separator
+                        + cyphFiles[i];
+
+                    OutputStream OS = null;
+                    InputStream IS = null;
+                    try {
+                        // Use asset manager to fetch a file-descriptor for the new file
+                        // and use that to open an InputStream (needed by most copy methods)
+                        IS = mngr.openFd(
+                                "cipher-library"
+                                + File.separator
+                                + cyphFiles[i])
+                            .createInputStream();
+                        if (IS == null) {
+                            throw new IOException("IS is null");
+                        }
+                        OS = new FileOutputStream(new File(destFile));
+                        if (OS == null) {
+                            throw new IOException("OS is null");
+                        }
+
+                        // Apparently only the crappy primitive method works here
+                        byte[] buf = new byte[1024];
+                        int len;
+
+                        while ((len = IS.read(buf)) > 0) {
+                            OS.write(buf, 0, len);
+                        }
+
+                        IS.close();
+                        OS.close();
+                    }
+                    catch (IOException ex) {
+                        ex.printStackTrace();
+                        throw ex;
+                    }
+                    finally {
+                        if (OS != null)
+                            OS.close();
+                        if (IS != null)
+                            IS.close();
+                    }
+                }
+            }
+            catch(IOException ex){
+                Log.e(TAG, "Failed to install files properly");
+                Log.e(TAG, ex.getMessage());
+                finish();
+            }
+
+            prefs.edit().putBoolean("FIRST_RUN", false).commit();
+        //}
+
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+}
